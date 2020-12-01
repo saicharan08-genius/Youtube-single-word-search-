65d5afdeab8da5ee9e6dd6d38700cf30e10b60a9
Saicharan Mugunthan <70827777+saicharan08-genius@users.noreply.github.com>
2020-12-01 10:15:53 +0530

    Create Search.cpp

---
+++ Search.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string input;
+    cin >> input;
+    cout << "https://www.youtube.com/results?search_query=" << input;
+
+    return 0;
+}
